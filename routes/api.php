046ce06f78db57afe501c2f6b8de5b7da7fc8c85
doobie-droid <lesliedouglas23@gmail.com>
2025-03-26 13:12:30 +0100

    feat: add routes for product, show, index and create routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('v1')->name('v1.')->group(function () {
+    Route::prefix('products')->name('products.')->group(function () {
+        Route::get('/', [ProductController::class, 'index'])->name('index');
+        Route::get('/{id}', [ProductController::class, 'show'])->name('show');
+        Route::post('/', [ProductController::class, 'store'])->name('store');
+    });
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

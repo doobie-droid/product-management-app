8d2d061815a08e0db7346645500a180e4596de34
doobie-droid <lesliedouglas23@gmail.com>
2025-03-26 23:20:07 +0100

    docs: update ProductFactory to include scribe state for deterministic output in scribe documentation

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -22,4 +22,19 @@ class ProductFactory extends Factory
             "status" => rand(0, 1),
         ];
     }
+
+    /**
+     * This state is present for scribe documentation
+     */
+    public function scribe()
+    {
+        return $this->state([
+            'id' => 1,
+            'name' => 'Scribe Product',
+            'category' => 'electronics',
+            'status' => 'active',
+            'created_at' => "2025-03-26T21:52:20.000000Z",
+            'updated_at' => "2025-03-26T21:52:20.000000Z",
+        ]);
+    }
 }

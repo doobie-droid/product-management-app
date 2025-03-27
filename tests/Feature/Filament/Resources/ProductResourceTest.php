4af6114ce7efd101de4e056f3e09b56fecce1dd8
doobie-droid <lesliedouglas23@gmail.com>
2025-03-27 05:12:00 +0100

    test: add test for Product resource to check that only admin users can edit and delete columns

--- tests/Feature/Filament/Resources/ProductResourceTest.php
+++ tests/Feature/Filament/Resources/ProductResourceTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Filament\Resources\ProductResource;
 use App\Filament\Resources\ProductResource\Pages\ManageProducts;
+use App\Models\Product;
 use App\Models\User;
 use function Pest\Livewire\livewire;
 
@@ -11,4 +13,31 @@ beforeEach(function () {
 
 it('can render page for managing products', function () {
     livewire(ManageProducts::class)->assertSuccessful();
+});
+
+it('shows the edit and delete columns for admin users', function () {
+    $product = Product::factory()->create();
+
+    livewire(ManageProducts::class)
+        ->assertTableColumnVisible('name')
+        ->assertTableColumnVisible('category')
+        ->assertTableColumnVisible('status');
+
+    livewire(ManageProducts::class)
+        ->assertTableActionVisible('edit', $product)
+        ->assertTableActionVisible('delete', $product);
+});
+
+it('does not shows the edit and delete columns for admin users', function () {
+    $this->actingAs(User::factory()->create());
+    $product = Product::factory()->create();
+
+    livewire(ManageProducts::class)
+        ->assertTableColumnVisible('name')
+        ->assertTableColumnVisible('category')
+        ->assertTableColumnVisible('status');
+
+    livewire(ManageProducts::class)
+        ->assertTableActionHidden('edit', $product)
+        ->assertTableActionHidden('delete', $product);
 });
